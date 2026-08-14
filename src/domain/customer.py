3d78fe50cf4431f4e63872c8c5cd2496273fc7e0
Lucas <lucascrespibarbosa@gmail.com>
2026-08-14 13:08:54 -0300

    customer added

---
+++ src/domain/customer.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+import os
+
+@dataclass
+class Customer:
+    id: str
+    name: str
+
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src.domain.product import Product
 from src.domain.order import Order
+from src.domain.customer import Customer
 
 if __name__ == "__main__":
     print("Hello World!")
